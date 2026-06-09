5304e6bdcadfa6efbc3ded581ad87c22ea56cf54
BlueDragon4251 <136262109+BlueDragon4251@users.noreply.github.com>
2026-06-09 19:27:29 +0200

    first commit

---
+++ src/Providers/MinecraftToolkitPluginProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace BlueWolf\MinecraftToolkit\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class MinecraftToolkitPluginProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+}
